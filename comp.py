fcd9ca918b4cb0962312dd2a753661dd9b48f4ce
Hassan Afzaal <83668567+hafzaal@users.noreply.github.com>
2024-09-26 16:58:49 +0000

    Create recursive function to lookup subfolders

--- comp.py
+++ comp.py
@@ -8,20 +8,37 @@ DESTINATION_BUCKET: str = "b1-ncloud-dev"
 SOURCE_PROFILE: str = "prod"
 DESTINATION_PROFILE: str = "dev"
 
+# Initial call is with prefix "" and delimiter
+# From initial call, we will get a response that will contain CommonPrefixes, that will contain all the subfolders.
+# We then go through each common prefix/subfolder and make the call again but this time, prefix = commonPrefix and delim = /
+# while, response.commonprefix isn't empty
+# We keep doing this until we have all the subfolders.
+
+def lookup_subfolders(prefix: str, bucket_name: str, s3_client: S3Client, folder_paths: set[str]) -> None:
+    response = s3_client.list_objects_v2(Bucket=bucket_name,
+                                         Prefix=prefix,
+                                         Delimiter='/')
+
+    subfolders = response.get("CommonPrefixes", None) # pyright: ignore [reportTypedDictNotRequiredAccess]
+    if subfolders is None:
+        return
+    for folder in subfolders:
+        folder_name: str = folder["Prefix"] # pyright: ignore [reportTypedDictNotRequiredAccess]
+        folder_paths.add(folder_name)
+        lookup_subfolders(folder_name, bucket_name, s3_client, folder_paths)
+
 
 def get_bucket_folders(bucket_name: str, s3_client: S3Client) -> set[str]:
     folder_paths: set[str] = set()
 
-    response = s3_client.list_objects_v2(Bucket=bucket_name)
-    if "Contents" not in response:
-        print(f"The bucket <{bucket_name}> does not contain any folders.")
-        return folder_paths
-    
-    for obj in response["Contents"]:
-        folder_path: str = obj["Key"] # pyright: ignore [reportTypedDictNotRequiredAccess]
-        if folder_path.endswith('/'):
-            folder_paths.add(folder_path)
+    # response = s3_client.list_objects_v2(Bucket=bucket_name,
+    #                                      Prefix="",
+    #                                      Delimiter='/')
+    # subfolders = response["CommonPrefixes"] # pyright: ignore [reportTypedDictNotRequiredAccess]
 
+    # if subfolders:
+    #     prefix: Any = next(iter(subfolders))
+    lookup_subfolders("", bucket_name, s3_client, folder_paths)
     return folder_paths
 
 def compare_bucket_folders(source_folders: set[str], dest_folders: set[str]) -> list[str]:
@@ -63,8 +80,9 @@ def main() -> None:
     prod_session = boto3.Session(profile_name=SOURCE_PROFILE)
     s3_prod = prod_session.client('s3')
 
-    print(sync_buckets(s3_prod, s3_dev))
-    print(sync_buckets(s3_prod, s3_dev))
+    #print(sync_buckets(s3_prod, s3_dev))
+    #print(sync_buckets(s3_prod, s3_dev))
+    print(get_bucket_folders(SOURCE_BUCKET, s3_prod))
 
 
 if __name__ == "__main__":
