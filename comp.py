27efb18563e8ad6ec6e59fdc1dac33ef5527f976
Hassan Afzaal <83668567+hafzaal@users.noreply.github.com>
2024-10-03 22:39:29 +0000

    Add docstrings for functions. Remove root_folder parameter from get_bucket_folder

--- comp.py
+++ comp.py
@@ -3,6 +3,15 @@ from mypy_boto3_s3.client import S3Client
 from config import *
 
 def lookup_subfolders(current_folder: str, bucket_name: str, s3_client: S3Client, folder_paths: set[str], root_folder: str) -> None:
+    """A recursive function that is used to find all folders and subfolders in a bucket.
+
+    Args:
+        current_folder (str): The current folder where the method searches for further subfolders.
+        bucket_name (str): The bucket which contains folders that need to be retrieved.
+        s3_client (S3Client): The client whose bucket needs to be searched.
+        folder_paths (set[str]): A set containing all the currently found folders/paths.
+        root_folder (str): The root folder where the search is initiated.
+    """
     paginator = s3_client.get_paginator('list_objects_v2')
     for page in paginator.paginate(Bucket=bucket_name, Prefix=current_folder, Delimiter='/'):
         subfolders = page.get("CommonPrefixes", None) # pyright: ignore [reportTypedDictNotRequiredAccess]
@@ -16,16 +25,43 @@ def lookup_subfolders(current_folder: str, bucket_name: str, s3_client: S3Client
                 folder_paths.add(folder_name)
             lookup_subfolders(folder_name, bucket_name, s3_client, folder_paths, root_folder)
 
-def get_bucket_folders(starting_folder: str, bucket_name: str, s3_client: S3Client, root_folder: str) -> set[str]:
+def get_bucket_folders(starting_folder: str, bucket_name: str, s3_client: S3Client) -> set[str]:
+    """Gets all folders from inside a bucket by recursively searching through each folder based on starting folder specified.
+
+    Args:
+        starting_folder (str): The folder where the search begins. Pass empty string to denote root of bucket, otherwise specify the folder name.
+        bucket_name (str): The bucket which contains folders that need to be retrieved.
+        s3_client (S3Client): The client whose bucket needs to be searched.
+
+    Returns:
+        set[str]: _description_
+    """
     folder_paths: set[str] = set()
+    root_folder: str = starting_folder
     lookup_subfolders(starting_folder, bucket_name, s3_client, folder_paths, root_folder)
     return folder_paths
 
 def compare_bucket_folders(source_folders: set[str], dest_folders: set[str]) -> list[str]:
+    """Compares two sets of folders and finds the difference between them.
+
+    Args:
+        source_folders (set[str]): A set of strings denoting folder names/path of all folders in the source bucket.
+        dest_folders (set[str]): A set of strings denoting folder names/path of all folders in the destination bucket.
+
+    Returns:
+        list[str]: A list that contains folders present in the source bucket but missing from the destination bucket.
+    """
     missing_folders: list[str] = list(source_folders - dest_folders)
     return missing_folders
 
 def add_missing_folders(bucket_name: str, missing_folders: list[str], s3_client: S3Client) -> None:
+    """Add missing folders to a client's bucket.
+
+    Args:
+        bucket_name (str): The name of the bucket to which folders will be added.
+        missing_folders (list[str]): The list of folder names/path which are missing in the bucket.
+        s3_client (S3Client): The client whose bucket the folders will be copied to.
+    """
     count: int = 0
     if not missing_folders:
         print(f"All folders already exist in bucket <{bucket_name}>.")
@@ -38,16 +74,24 @@ def add_missing_folders(bucket_name: str, missing_folders: list[str], s3_client:
     print(f"{count} folders added to bucket:{bucket_name}")
 
 def sync_buckets(source_s3_client: S3Client, destination_s3_client: S3Client) -> list[str]:
+    """ Copies and Syncs folders from a source bucket to destination bucket.
+
+    Args:
+        source_s3_client (S3Client): A client referencing the source bucket. Data will be copied from this client's bucket.
+        destination_s3_client (S3Client): A client referencing the destination bucket. Data will be copied to this client's bucket.
+
+    Returns:
+        list[str]: Returns a list of strings denoting the paths/folder names of every folder that was present in source folder
+        but is missing in the destination folder.
+    """
     print("Getting source bucket folders...")
     source_folders: set[str] = get_bucket_folders(starting_folder=SOURCE_DIRECTORY.NAME,
                                                   bucket_name=SOURCE_BUCKET,
-                                                  s3_client=source_s3_client,
-                                                  root_folder=SOURCE_DIRECTORY.NAME)
+                                                  s3_client=source_s3_client)
     print("Getting destination bucket folders...")
     dest_folders: set[str] = get_bucket_folders(starting_folder=DESTINATION_DIRECTORY.NAME,
                                                 bucket_name=DESTINATION_BUCKET,
-                                                s3_client=destination_s3_client,
-                                                root_folder=DESTINATION_DIRECTORY.NAME)
+                                                s3_client=destination_s3_client)
     print("Finding missing folders...")
     missing_folders: list[str] = compare_bucket_folders(source_folders, dest_folders)
     
